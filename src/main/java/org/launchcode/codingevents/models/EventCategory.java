931e0d44e358ceeb1ac831686d6e703f6defca7c
Rebecca Pavelka <pavelka6@sbcglobal.net>
2020-01-25 14:13:22 -0600

    After Ch18 video 1

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,11 +1,8 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
 public class EventCategory extends AbstractEntity {
